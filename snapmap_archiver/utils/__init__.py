f8da54c8b461852e87f0297288dc626b971d3909
king-millez <millez.dev@gmail.com>
2021-01-29 13:21:36 +1100

    [downloader] skip duplicates

--- snapmap_archiver/utils/__init__.py
+++ snapmap_archiver/utils/__init__.py
@@ -1,4 +1,4 @@
-import sys, json, subprocess
+import sys, json, subprocess, os
 
 def organise_media(api_data):
     to_download = []
@@ -19,7 +19,9 @@ def organise_media(api_data):
     return(to_download)
 
 def download_media(output_dir, organised_data, dl_json=False):
-    for snap in organised_data:
+    for index,snap in enumerate(organised_data):
+        DL_MSG = f'Snap {index + 1}/{len(organised_data)} downloading...'
+
         if(dl_json):
             with open(f'{output_dir}/{snap["id"]}.info.json', 'w') as json_file:
                 json_file.write(json.dumps(snap, indent=2))
@@ -29,6 +31,14 @@ def download_media(output_dir, organised_data, dl_json=False):
             cmd = ['aria2c', snap['media']['raw_url'], '-d', output_dir, '-o']
 
         if(snap['media']['raw_url'][-3:] == 'mp4'):
-            subprocess.run(cmd + [snap['id'] + '.mp4'])
+            if(os.path.exists(f'{cmd[-2]}/{snap["id"]}.mp4')):
+                print(f'Snap {index + 1}/{len(organised_data)} already downloaded.')
+            else:
+                print(DL_MSG + f' - {snap["id"]}.mp4')
+                subprocess.run(cmd + [snap['id'] + '.mp4'], capture_output=True)
         else:
-            subprocess.run(cmd + [snap['id'] + '.jpg'])
+            if(os.path.exists(f'{cmd[-2]}/{snap["id"]}.jpg')):
+                print(f'Snap {index + 1}/{len(organised_data)} already downloaded.')
+            else:
+                print(DL_MSG + f' - {snap["id"]}.jpg')
+                subprocess.run(cmd + [snap['id'] + '.jpg'], capture_output=True)
