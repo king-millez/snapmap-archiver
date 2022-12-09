a3532d8897739d7ae23338783b81287102126ea9
king-millez <millez.dev@gmail.com>
2022-12-09 20:11:56 +1100

    Very minor formatting fixes

--- setup.py
+++ setup.py
@@ -3,34 +3,30 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='snapmap-archiver',
-    version='1.3.0',
-    description='Download all Snapmaps content from a specific location.',
+    name="snapmap-archiver",
+    version="1.3.1",
+    description="Download all Snapmaps content from a specific location.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/king-millez/snapmap-archiver',
-    author='king-millez',
-    author_email='millez.dev@gmail.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/king-millez/snapmap-archiver",
+    author="king-millez",
+    author_email="millez.dev@gmail.com",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
     packages=find_packages(),
     include_package_data=True,
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-        'certifi',
-        'chardet',
-        'idna',
-        'requests',
-        'urllib3',
+        "certifi",
+        "chardet",
+        "idna",
+        "requests",
+        "urllib3",
     ],
-    entry_points={
-        "console_scripts": [
-            "snapmap-archiver=snapmap_archiver:main"
-        ]
-    }
+    entry_points={"console_scripts": ["snapmap-archiver=snapmap_archiver:main"]},
 )
--- snapmap_archiver/get_data.py
+++ snapmap_archiver/get_data.py
@@ -1,82 +1,70 @@
-from typing import Dict, List
-import requests
-import random
-import json
-import pathlib
 import sys
-
-
-req_headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240',
-    'Host': 'ms.sc-jpl.com',
-    'Accept': '*/*',
-    'Accept-Language': 'en-US',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://map.snapchat.com/@0,0,7.67z',
-    'Content-Type': 'application/json',
-    'Origin': 'https://map.snapchat.com',
-    'Content-Length': '2',
-    'Connection': 'keep-alive',
-    'DNT': '1',
-    'Sec-GPC': '1',
-    'TE': 'Trailers',
-}
+import json
+import requests
+from typing import Dict, List
 
 
 def get_epoch():
-    for entry in json.loads(requests.post(
-        'https://ms.sc-jpl.com/web/getLatestTileSet',
-            headers=req_headers, json={}).text)['tileSetInfos']:
-        if(entry['id']['type'] == 'HEAT'):
-            return(entry['id']['epoch'])
+    for entry in json.loads(
+        requests.post(
+            "https://ms.sc-jpl.com/web/getLatestTileSet",
+            headers={"Content-Type": "application/json"},
+            json={},
+        ).text
+    )["tileSetInfos"]:
+        if entry["id"]["type"] == "HEAT":
+            return entry["id"]["epoch"]
 
 
-def api_query(lat: float = 0, lon: float = 0.0, zl: int = 5, max_radius: int = 10000, mode: str = 'loc', snap_ids: list = []) -> Dict[str, List]:
-    if mode == 'loc':
+def api_query(
+    lat: float = 0,
+    lon: float = 0.0,
+    zl: float = 5.72,
+    max_radius: int = 30000,
+    mode: str = "loc",
+    snap_ids: list = [],
+) -> Dict[str, List]:
+    if mode == "loc":
         available_snaps = []
         current_iteration = max_radius
         _epoch = get_epoch()
         try:
-            print('Querying Snaps...')
+            print("Querying Snaps...")
             while current_iteration != 1:
-                print(f'Querying with radius {current_iteration}...')
+                print(f"Querying with radius {current_iteration}...")
                 payload = {
-                    "requestGeoPoint": {
-                        "lat": lat,
-                        "lon": lon
-                    },
+                    "requestGeoPoint": {"lat": lat, "lon": lon},
                     "zoomLevel": zl,
-                    "tileSetId": {
-                        "flavor": "default",
-                        "epoch": _epoch,
-                        "type": 1
-                    },
+                    "tileSetId": {"flavor": "default", "epoch": _epoch, "type": 1},
                     "radiusMeters": current_iteration,
-                    "maximumFuzzRadius": 0
+                    "maximumFuzzRadius": 0,
                 }
-                req_headers['Content-Length'] = str(len(str(payload)))
-                api_data = requests.post('https://ms.sc-jpl.com/web/'
-                                         'getPlaylist', headers=req_headers,
-                                         json=payload).json()
-                available_snaps = available_snaps + \
-                    api_data['manifest']['elements']
-                if(current_iteration > 2000):
+                api_data = requests.post(
+                    "https://ms.sc-jpl.com/web/" "getPlaylist",
+                    headers={
+                        "Content-Type": "application/json",
+                    },
+                    json=payload,
+                ).json()
+                available_snaps = available_snaps + api_data["manifest"]["elements"]
+                if current_iteration > 2000:
                     current_iteration = current_iteration - 2000
-                elif(current_iteration > 1000):
+                elif current_iteration > 1000:
                     current_iteration = current_iteration - 100
                 else:
                     current_iteration = 1
-            print('Sorting list of Snaps...')
-            return [i for n, i in enumerate(available_snaps)
-                    if i not in available_snaps[n + 1:]]
+            print("Sorting list of Snaps...")
+            return [
+                i
+                for n, i in enumerate(available_snaps)
+                if i not in available_snaps[n + 1 :]
+            ]
         except Exception:
-            sys.exit("You seem to have been rate limited, "
-                     "please wait and try again.")
+            sys.exit(
+                "You seem to have been rate limited, " "please wait and try again."
+            )
 
-    if mode == 'snap':
+    if mode == "snap":
         return requests.post(
-            'https://ms.sc-jpl.com/web/getStoryElements',
-            json={
-                "snapIds": snap_ids
-            }
+            "https://ms.sc-jpl.com/web/getStoryElements", json={"snapIds": snap_ids}
         ).json()
