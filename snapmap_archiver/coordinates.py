3ddd7d755b5ad27a68cbda577791053653b1b3cc
king-millez <millez.dev@gmail.com>
2023-01-28 14:37:38 +1100

    [2/2] Add corrected filename

---
+++ snapmap_archiver/coordinates.py
@@ -0,0 +1,23 @@
+class Coordinates:
+    def __init__(self, coord_str: str):
+        self.geo_msg = (
+            '\n\nUse comma seperated values for latitude/longitude, e.g: -l="35.0,67.0"'
+        )
+        if "," not in coord_str:
+            raise ValueError(
+                f"No comma is present in the provided coordinates.{self.geo_msg}"
+            )
+        try:
+            lat, long = coord_str.split(",", 1)
+            self.lat = float(lat)
+            self.long = float(long)
+        except Exception:
+            raise ValueError(
+                f"Provided coordinates could not be split to lat/long points.{self.geo_msg}"
+            )
+
+    def __str__(self) -> str:
+        return f"Lat: {self.lat}, Lon: {self.long}"
+
+    def __repr__(self) -> str:
+        return f"({self.lat},{self.long})"
